fe21b9ce67f0e9c9a5568d8fa2c66d7c690d37bf
Corey Werner <cnotethegr8@gmail.com>
2019-04-07 14:59:45 +0300

    remove pods for repo

--- KinBackupRestoreModule/Pods/Target Support Files/KinBackupRestoreModule/KinBackupRestoreModule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KinBackupRestoreModule : NSObject
-@end
-@implementation PodsDummy_KinBackupRestoreModule
-@end
--- KinBackupRestoreModule/Pods/Target Support Files/Pods-KinBackupRestoreModuleTests/Pods-KinBackupRestoreModuleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KinBackupRestoreModuleTests : NSObject
-@end
-@implementation PodsDummy_Pods_KinBackupRestoreModuleTests
-@end
